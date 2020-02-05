7eecc4cf0ca82d4e9e3d3578a67ef314d79cc199
Summer <coconamy7727@naver.com>
2020-02-05 19:47:07 +0900

    try webRTC

--- vue-front/front-sk/src/views/general/GameRoom.vue
+++ vue-front/front-sk/src/views/general/GameRoom.vue
@@ -6,14 +6,19 @@
       <!-- https://xyxufvchfhks3520734.cdn.ntruss.com/video/ls-20200122105535-tD4G9_1080p_a_l.m3u8 -->
       <div id="firstWebCam">
         firstWebCam 들어갈자리
-        <video
+       <!-- <video playsinline id ="left_cam" controls preload="metadata" autoplay> -->
+
+         <video autoplay playsinline>
+         </video>
+         
+        <!-- <video
           autoplay="autoplay"
           controls="controls"
           style="width:50%"
           playsinline
-        >
+        > -->
           <!-- <source src="https://xyxufvchfhks3520734.cdn.ntruss.com/video/ls-20200122105535-tD4G9_1080p_a_l.m3u8" type="application/x-mpegURL" /> -->
-        </video>
+        <!-- </video> -->
         <!-- <object
         src="https://xyxufvchfhks3520734.cdn.ntruss.com/video/ls-20200122105535-tD4G9_1080p_a_l.m3u8"
         ></object>-->
@@ -25,6 +30,7 @@
       </div>
     </div>
 
+<webchat/>
     <div id="Chat">
       채팅
       <input type="text" placeholder="보낼 메세지를 입력하세요." />
@@ -34,13 +40,44 @@
 </template>
 
 <script>
+
+// const localVideo = document.getElementById('left_cam');
+// navigator.mediaDevices
+//       .getUserMedia({
+//         audio: true,
+//         video: true
+//       })
+//       .then(this.getStream)
+//       .catch(function(e){
+//         alert('getUserMedia() error:'+e.name);
+//       });
+
+import webchat from './webChat.js';
+
 export default {
+  
   name: "GameRoom",
-  components: {},
+  components: {webchat},
   data() {
     return {
+      //localVideo:null,
       message: "hello tess"
     };
   }
-};
+  
+  // methods:{
+  //    async getStream(stream) {
+  //      console.log('Adding local stream');
+  // //this.localVideo = await document.getElementById("local_video");
+  //     this.localVideo.srcObject = stream;
+  //     this.localStream = stream;
+  //     if (isInitiator){
+  //       maybeStart();
+  //     }
+  //   },
+  //   maybeStart(){
+  //     console.log('>>>>>>>>>>>>maybeStart()',isStarted, localStream, isChannelReady);
+  //   }
+  // }
+ };
 </script>
